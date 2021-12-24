ac895744df333e2e28ea495e270fa68f20444c4e
Safrin La Batu <batus@cardiff.ac.uk>
2021-12-24 21:41:50 +0800

    initial commit

---
+++ App.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById("menu");
+const hamburger = document.getElementById("hamburger");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("show")
+})
